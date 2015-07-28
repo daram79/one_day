6df328fab8dd7c302bf5647a896a66359ea3607e
daram79 <mayin1q84@gmail.com>
2015-07-28 23:50:23 +0900

    edit user table

--- db/migrate/20150726093631_create_users.rb
+++ db/migrate/20150726093631_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.text :registration_id
+      t.boolean   :alram_on, default: true
       t.timestamps
     end
   end
