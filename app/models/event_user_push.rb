f7a9b7c264e024ff35ef123761450f6ca0e9e817
daram79 <mayin1q84@gmail.com>
2015-12-01 21:25:05 +0900

    푸시 메시지 추가

--- app/models/event_user_push.rb
+++ app/models/event_user_push.rb
@@ -5,8 +5,11 @@ class EventUserPush < ActiveRecord::Base
     Thread.new do
       gcm = GCM.new("AIzaSyD_3jJfuO8NT8G-kDHcmTiwl3w0W1JuxXQ")
       user_ids = EventUserPush.where(send_flg: false).group(:event_user_id).pluck(:event_user_id)
+      debugger
+      event = Event.find(self.event_id)
+      option = { :data => {'message' => event.event_name} }
       registration_ids = EventUserRegistrations.where(event_user_id: user_ids).pluck(:registration_id)
-      gcm.send registration_ids unless registration_ids.blank?
+      gcm.send(registration_ids, option) unless registration_ids.blank?
     end
   end
 end
