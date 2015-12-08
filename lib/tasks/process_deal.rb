93f6dabd9f2f848d5cbf8ab728176d97ad401fe1
daram79 <mayin1q84@gmail.com>
2015-12-09 03:40:20 +0900

    edit

--- lib/tasks/process_deal.rb
+++ lib/tasks/process_deal.rb
@@ -14,34 +14,80 @@ browser = Watir::Browser.new
     # p "start add key 2"
     # DealSearchWord.add_key_coupang(browser)
     
-    # search_key = DealSearchWord.all.pluck(:word)
+    search_key = DealSearchWord.all.pluck(:word)
     
-    # #위메프
-    # p "start 1"
-    # DealItem.add_wemakeprice(browser, search_key)
+    #위메프
+    p "위메프"
+    ret = DealItem.add_wemakeprice(browser, search_key)
+    unless ret
+      browser.close
+      headless.destroy
+      
+      headless = Headless.new
+      headless.start
+      browser = Watir::Browser.new
+    end
     
-    # #쿠팡
-    # p "start 2"
-    # DealItem.add_coupang(browser, search_key)
-#     
-    # # #G9
-    # p "start 3"
-    # DealItem.add_g9(browser, search_key)
-#     
-    # # #쇼킹딜
-    # p "start 4"
-    # DealItem.add_shocking_deal(browser, search_key)
-#     
-    # # #티몬
-    # p "start 5"
-    # DealItem.add_tmon(browser, search_key)
-#     
-    p "megabox"
-    DealItem.movie_event_megabox(browser)
+    #쿠팡
+    p "쿠팡"
+    ret = DealItem.add_coupang(browser, search_key)
+    unless ret
+      browser.close
+      headless.destroy
+      
+      headless = Headless.new
+      headless.start
+      browser = Watir::Browser.new
+    end
+    
+    #G9
+    p "G9"
+    ret = DealItem.add_g9(browser, search_key)
+    unless ret
+      browser.close
+      headless.destroy
+      
+      headless = Headless.new
+      headless.start
+      browser = Watir::Browser.new
+    end
+    
+    #쇼킹딜
+    p "쇼킹딜"
+    ret = DealItem.add_shocking_deal(browser, search_key)
+    unless ret
+      browser.close
+      headless.destroy
+      
+      headless = Headless.new
+      headless.start
+      browser = Watir::Browser.new
+    end
+    
+    #티몬
+    p "티몬"
+    ret = DealItem.add_tmon(browser, search_key)
+    unless ret
+      browser.close
+      headless.destroy
+      
+      headless = Headless.new
+      headless.start
+      browser = Watir::Browser.new
+    end
+    
+    p "메가박스"
+    ret = DealItem.movie_event_megabox(browser)
+    unless ret
+      browser.close
+      headless.destroy
+      
+      headless = Headless.new
+      headless.start
+      browser = Watir::Browser.new
+    end
     
   end
-browser.close
-headless.destroy
 
 
 
