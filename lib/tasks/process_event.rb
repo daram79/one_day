4273512b5041db171a43656d87fdcb3dd3947878
daram79 <mayin1q84@gmail.com>
2015-12-10 00:50:20 +0900

    edit

--- lib/tasks/process_event.rb
+++ lib/tasks/process_event.rb
@@ -4,11 +4,12 @@ require "#{File.dirname(__FILE__)}/../../config/environment.rb"
 keys = DealSearchResult.all.group("deal_search_word").pluck(:deal_search_word)
 
 keys.each do |key|
-  datas = DealSearchResult.joins(:deal_item).where(deal_search_word: key).where("deal_items.deal_count > 5000")
+  datas = DealSearchResult.joins(:deal_item).where(deal_search_word: "레스토랑").where("deal_items.deal_count > 5000")
 
   datas.each do |data|
     word_data = DealSearchWord.find_by_word(data.deal_search_word)
-    Event.create(event_id: data.deal_item.item_id, event_name: data.deal_item.deal_title, event_url: data.deal_item.deal_url, event_site_id: word_data.id, show_flg: true)
+    event = Event.where(event_id: data.deal_item.item_id, event_site_id: word_data.id)
+    Event.create(event_id: data.deal_item.item_id, event_name: data.deal_item.deal_title, event_url: data.deal_item.deal_url, event_site_id: word_data.id, show_flg: true) if event.blank?
   end
 end
   
