c911ecac40cccf33811c8bdf9916c17f53e882d5
daram79 <mayin1q84@gmail.com>
2015-12-20 01:33:12 +0900

    edit

--- lib/tasks/check_event.rb
+++ lib/tasks/check_event.rb
@@ -2,7 +2,6 @@
 require "#{File.dirname(__FILE__)}/../../config/environment.rb"
 
 #deal_item데이터를 필터해서 event에 추가함.
-  while 1
     begin
       # ids = [1001, 1002, 1003, 1005, 9001]
       ids = [1001, 1002, 1005, 9001]
@@ -43,4 +42,3 @@ require "#{File.dirname(__FILE__)}/../../config/environment.rb"
     rescue
       p e.backtrace
     end
-  end
