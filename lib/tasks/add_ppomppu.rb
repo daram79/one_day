331eb99c19549d03415c3656d15d6da0bba11025
daram79 <mayin1q84@gmail.com>
2016-02-05 00:05:02 +0900

    edit

--- lib/tasks/add_ppomppu.rb
+++ lib/tasks/add_ppomppu.rb
@@ -2,6 +2,7 @@
 require "#{File.dirname(__FILE__)}/../../config/environment.rb"
 
 while true
+  p "start"
   head_url = "http://www.ppomppu.co.kr/zboard/"
   url = "http://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu"
   html_str = open(url).read
@@ -39,42 +40,40 @@ while true
     rescue
     end
   end
-end
 
-
-
-
-#자게
-urls = ["http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=30&category=&search_type=sub_memo&keyword=%C0%CC%BA%A5%C6%AE&x=0&y=0", #이벤트
-  "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=%C6%BC%B8%F3&x=0&y=0", #티몬
-  "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=%C4%ED%C6%CE&x=0&y=0", #쿠팡
-  "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=G9&x=22&y=15", #G9
-  "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=%C1%F6%B8%B6%C4%CF&x=0&y=0", #지마켓
-  "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=G%B8%B6%C4%CF&x=0&y=0", #G마켓
-  "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=11%B9%F8%B0%A1&x=0&y=0", #11번가
-  "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=%BF%C1%BC%C7&x=0&y=0", #옥션
-  "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=%B7%D4%B5%A5%B8%B6%C6%AE&x=0&y=0",#롯데마트
-  "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=%C0%CC%B8%B6%C6%AE&x=0&y=0", #이마트
-  "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=%C8%A8%C7%C3&x=20&y=13" #홈플
-  ]
-  
-urls.each do |url|
-  html_str = open(url).read
-doc = Nokogiri::HTML(html_str)
-items = doc.css("#revolution_main_table tr")
-  items.each do |item|
-    begin
-      next unless item.attr("class")
-      category_id = 2
-      item_id = item.css("td")[2].css("a").attr("href").value.split("=")[-1]
-      search_ret = Ppomppu.where(category_id: category_id, item_id: item_id)
-      next unless search_ret.blank?
-      
-      
-      title = item.css("td")[2].css("a").text
-      link_url = head_url + item.css("td")[2].css("a").attr("href").value
-      Ppomppu.create(category_id: category_id, item_id: item_id, title: title, url: link_url)
-    rescue
+  #자게
+  urls = ["http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=30&category=&search_type=sub_memo&keyword=%C0%CC%BA%A5%C6%AE&x=0&y=0", #이벤트
+    "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=%C6%BC%B8%F3&x=0&y=0", #티몬
+    "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=%C4%ED%C6%CE&x=0&y=0", #쿠팡
+    "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=G9&x=22&y=15", #G9
+    "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=%C1%F6%B8%B6%C4%CF&x=0&y=0", #지마켓
+    "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=G%B8%B6%C4%CF&x=0&y=0", #G마켓
+    "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=11%B9%F8%B0%A1&x=0&y=0", #11번가
+    "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=%BF%C1%BC%C7&x=0&y=0", #옥션
+    "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=%B7%D4%B5%A5%B8%B6%C6%AE&x=0&y=0",#롯데마트
+    "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=%C0%CC%B8%B6%C6%AE&x=0&y=0", #이마트
+    "http://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard&page_num=20&category=&search_type=sub_memo&keyword=%C8%A8%C7%C3&x=20&y=13" #홈플
+    ]
+    
+  urls.each do |url|
+    html_str = open(url).read
+  doc = Nokogiri::HTML(html_str)
+  items = doc.css("#revolution_main_table tr")
+    items.each do |item|
+      begin
+        next unless item.attr("class")
+        category_id = 2
+        item_id = item.css("td")[2].css("a").attr("href").value.split("=")[-1]
+        search_ret = Ppomppu.where(category_id: category_id, item_id: item_id)
+        next unless search_ret.blank?
+        
+        
+        title = item.css("td")[2].css("a").text
+        link_url = head_url + item.css("td")[2].css("a").attr("href").value
+        Ppomppu.create(category_id: category_id, item_id: item_id, title: title, url: link_url)
+      rescue
+      end
     end
   end
+p "end"
 end
