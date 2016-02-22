a3903141c48ddb52836aeee20d844323f3355741
daram79 <mayin1q84@gmail.com>
2016-02-22 23:25:12 +0900

    edit

--- app/models/event.rb
+++ app/models/event.rb
@@ -450,6 +450,40 @@ class Event < ActiveRecord::Base
     end
   end
   
+  
+  def self.super_ggul
+    begin
+      event_site_id = 9001
+      first_url = "http://m.ticketmonster.co.kr"
+      url = "http://m.ticketmonster.co.kr/"
+      
+      html_str = open(url).read
+      doc = Nokogiri::HTML(html_str)
+      
+      lis = doc.css("#flick_lst li")
+      lis.each do |li|
+        title = li.css(".thmb img").attr("alt").value
+        if title.include?("슈퍼꿀딜")
+          price = li.css(".sale em").text
+          price = price.scan(/\d/).join('').to_i
+          original_price = li.css(".prime em").text
+          discount = ""
+          rear_link_url = li.css("a")[0].attributes["href"].value
+          event_id = rear_link_url.split("/")[3].split("?")[0]
+          event_name = title
+          event_url = first_url + rear_link_url
+          event = Event.where(event_id: event_id, event_site_id: event_site_id)
+          image_url = li.css(".thmb img").attr("src").value
+          if event.blank?
+            Event.create(event_id: event_id, event_name: event_name, event_url: event_url, event_site_id: event_site_id, image_url: image_url, price: price, original_price: original_price, discount: discount, show_flg: true, push_flg: true, update_flg: true)
+          end
+        end
+      end
+    rescue => e
+      pp e.backtrace
+    end
+  end
+  
   # move => event_alram_mailer_watir
   # def self.get_g9_flash_deal(event_site_id)
     # begin
