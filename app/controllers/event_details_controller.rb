2984e176f5eb8dd269bda232563578efbe892a62
daram79 <mayin1q84@gmail.com>
2016-03-06 15:43:56 +0900

    edit

--- app/controllers/event_details_controller.rb
+++ app/controllers/event_details_controller.rb
@@ -24,20 +24,10 @@ class EventDetailsController < ApplicationController
   end
 
   # GET /event_details/new
-  def google_plus
-    client_secrets = Google::APIClient::ClientSecrets.load("config/client_secret.json")
-    auth_client = client_secrets.to_authorization
-    auth_client.update!( :scope => 'https://www.googleapis.com/auth/plus.media.upload', :redirect_uri => 'http://localhost:3000/event_details/new/' )
-    auth_uri = auth_client.authorization_uri.to_s
-    redirect_to auth_uri
-    
-  end
-    
-  
   def blog
     client_secrets = Google::APIClient::ClientSecrets.load("config/client_secret.json")
     auth_client = client_secrets.to_authorization
-    auth_client.update!( :scope => 'https://www.googleapis.com/auth/blogger', :redirect_uri => 'http://localhost:3000/event_details/new/' )
+    auth_client.update!( :scope => 'https://www.googleapis.com/auth/blogger', :redirect_uri => 'http://happyhouse.me:81/event_details/new/' )
     auth_uri = auth_client.authorization_uri.to_s
     redirect_to auth_uri
   end
