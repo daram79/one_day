f96d77a4255cf785db071f011c7a31b59589dc32
daram79 <mayin1q84@gmail.com>
2016-03-12 10:40:54 +0900

    edit

--- app/controllers/convenience_items_controller.rb
+++ app/controllers/convenience_items_controller.rb
@@ -76,14 +76,23 @@ class ConvenienceItemsController < ApplicationController
     keyword = params[:convenience_item_keyword][:keyword]
     master_id = params[:convenience_item_keyword][:convenience_master_id]
     ConvenienceItemKeyword.create(convenience_master_id: master_id, keyword: keyword)
-    redirect_to action: "add_keyword", master_id: master_id
+    if Rails.env == 'development'
+      redirect_to action: "add_keyword", master_id: master_id
+    else
+      redirect_to action: "add_keyword", master_id: master_id, :port => 81
+    end
+    
   end
   
   def delete_keyword
     master_id = params[:master_id]
     keyword_id = params[:keyword_id]
     ConvenienceItemKeyword.find(keyword_id).destroy
-    redirect_to action: "add_keyword", master_id: master_id
+    if Rails.env == 'development'
+      redirect_to action: "add_keyword", master_id: master_id
+    else
+      redirect_to action: "add_keyword", master_id: master_id, :port => 81
+    end
   end
   
   def search
