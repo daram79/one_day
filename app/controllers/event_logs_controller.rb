424c5ed73e0d4145e4c56bde9a1f6681a0a27160
daram79 <mayin1q84@gmail.com>
2016-03-15 14:38:14 +0900

    edit

--- app/controllers/event_logs_controller.rb
+++ app/controllers/event_logs_controller.rb
@@ -4,7 +4,85 @@ class EventLogsController < ApplicationController
   # GET /event_logs
   # GET /event_logs.json
   def index
-    @event_logs = EventLog.all
+    # @event_logs = EventLog.all
+    start_date = Time.now.beginning_of_day
+    end_date = Time.now.end_of_day
+    
+    #today
+    #dau
+    dau = EventLog.where("created_at between  ? and ?", start_date, end_date).pluck("event_user_id")
+    if dau.blank?
+      @dau_size = 0
+    else
+      @dau_size = dau.uniq!.size
+    end
+    
+    
+    @connect_user_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "connect_user").count
+    
+    
+    
+    #push 방문 접속자
+    @push_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "click_push").count
+    
+    #push 편의점 방문 접속자
+    @conveni_gs25_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "click_conveni_gs25").count
+    
+    
+    @conveni_seven_eleven_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "click_conveni_seven_eleven").count
+    
+    
+    @conveni_mini_stop_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "click_conveni_mini_stop").count
+    
+    
+    @conveni_cu_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "click_conveni_cu").count
+    
+    @conveni_search_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "click_conveni_search1+1").count
+    
+    @conveni_search_words = EventLog.where("created_at between  ? and ? and log_type = ?", start_date, end_date, "search_1+1").pluck(:action_type)
+    
+    @click_content_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "click_hotdeal_content").count
+    
+    
+    #yesterday
+    #dau
+    start_date = start_date.yesterday
+    end_date = start_date.yesterday
+    
+    
+    dau = EventLog.where("created_at between  ? and ?", start_date, end_date).pluck("event_user_id")
+    if dau.blank?
+      @yesterday_dau_size = 0
+    else
+      @yesterday_dau_size = dau.uniq!.size
+    end
+    
+    
+    @yesterday_connect_user_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "connect_user").count
+    
+    
+    
+    #push 방문 접속자
+    @yesterday_push_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "click_push").count
+    
+    #push 편의점 방문 접속자
+    @yesterday_conveni_gs25_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "click_conveni_gs25").count
+    
+    
+    @yesterday_conveni_seven_eleven_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "click_conveni_seven_eleven").count
+    
+    
+    @yesterday_conveni_mini_stop_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "click_conveni_mini_stop").count
+    
+    
+    @yesterday_conveni_cu_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "click_conveni_cu").count
+    
+    @yesterday_conveni_search_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "click_conveni_search1+1").count
+    
+    @yesterday_conveni_search_words = EventLog.where("created_at between  ? and ? and log_type = ?", start_date, end_date, "search_1+1").pluck(:action_type)
+    
+    @yesterday_click_content_size = EventLog.where("created_at between  ? and ? and action_type = ?", start_date, end_date, "click_hotdeal_content").count
+    
   end
 
   # GET /event_logs/1
